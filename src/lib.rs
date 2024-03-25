5a11f183dc8b3825661dc61518312b994dbd9896
EmperorOrokuSaki <artie.eth@gmail.com>
2024-03-25 13:47:49 +0400

    feat: move the function to the operations impl and write the parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use alloy_primitives::U256;
+use alloy_primitives::{Uint, U256};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Operations {
@@ -45,8 +45,192 @@ impl Operations {
             _ => Operations::OTHER(op.to_string()),
         }
     }
-}
+    pub fn parse_args(&self, stack: Option<Vec<U256>>) -> HashMap<String, String> {
+        let mut params: HashMap<String, String> = HashMap::new();
+        let unwrapped_stack = stack.as_ref().unwrap();
 
-pub fn parse_args(opcode: &Operations, stack: Option<Vec<U256>>) -> HashMap<String, String> {
-    HashMap::new()
+        // handle other changes that are applied to the already existing slots
+        match &self {
+            Operations::MCOPY => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Byte Size".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 3)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::MSTORE => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Value".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::EXTCODECOPY => {
+                //params.insert("Address".to_string(), unwrapped_stack.get(unwrapped_stack.len() - 1).unwrap().to_string());
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 3)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Byte Size".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 4)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::CODECOPY => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Byte Size".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 3)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::RETURNDATACOPY => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Byte Size".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 3)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::MSTORE8 => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+
+                params.insert(
+                    "Value".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::MLOAD => {
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            Operations::CALLDATACOPY => {
+                params.insert(
+                    "Destination Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 1)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Source Offset".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 2)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+                params.insert(
+                    "Byte Size".to_string(),
+                    unwrapped_stack
+                        .get(unwrapped_stack.len() - 3)
+                        .unwrap()
+                        .to::<u64>()
+                        .to_string(),
+                );
+            }
+            _ => {}
+        };
+        params
+    }
 }
